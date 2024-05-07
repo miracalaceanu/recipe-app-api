77575ed0b0530d164a335320e8f98c5372a86d20
miracalaceanu <miracalaceanu@gmail.com>
2024-05-07 14:12:24 +0300

    lint

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -64,7 +64,8 @@ class PrivateRecipeAPITests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = create_user(email="user@example.com", password="testpass123")
+        self.user = create_user(
+            email="user@example.com", password="testpass123")
         self.client.force_authenticate(self.user)
 
     def test_retrieve_recipes(self):
@@ -81,7 +82,8 @@ class PrivateRecipeAPITests(TestCase):
 
     def test_recipe_list_limited_to_user(self):
         """Test list of recipes is limited to authenticated user."""
-        other_user = create_user(email="other@example.com", password="wordpass123")
+        other_user = create_user(
+            email="other@example.com", password="wordpass123")
         create_recipe(user=other_user)
         create_recipe(user=self.user)
 
